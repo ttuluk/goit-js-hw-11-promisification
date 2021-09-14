34b989bce6bd8b23f83e80e03c050bbe80ecf146
ttuluk <ttasya.k@gmail.com>
2021-09-14 13:26:10 +0300

    add task 2

--- task-2.js
+++ task-2.js
@@ -3,33 +3,25 @@ const users = [
   { name: 'Poly', active: false },
   { name: 'Ajax', active: true },
   { name: 'Lux', active: false },
+
 ];
+const TIME = 1000;
 
 const toggleUserState = (allUsers, userName) => {
-    const users = allUsers.map(user => {
-    return new Promise(resolve => {
-    const time = 2000;
-
-    setTimeout(() => {
-        resolve({ name, active });
-    }, time);
-//     const updatedUsers = allUsers.map(user =>
-//     user.name === userName ? { ...user, active: !user.active } : user,
-//   );
-});
-})
-}
+  const users = allUsers.map(user => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user.name === userName) {
+        resolve({ ...user, active: !user.active });
+        };
+       resolve(user); }, TIME);
+      });
+    })
+    return Promise.all(users);
+  }
 
 const logger = updatedUsers => console.table(updatedUsers);
-toggleUserState(users, 'Mango').then(logger);
-/*
- * Сейчас работает так
- */
-// toggleUserState(users, 'Mango', logger);
-// toggleUserState(users, 'Lux', logger);
 
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
+
